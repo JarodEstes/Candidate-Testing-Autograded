dce8231a05936b488d7032b94e165273ddfc065b
JarodEstes <estesjarod48@gmail.com>
2024-01-21 22:19:14 -0600

    candidate-testing p2

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,14 +5,16 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who was the first American woman in space? ";
+let question = "Who was the first American woman in space?";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
+let questions = ["Who was the first American woman in space?", "True or false: 5 kilometer == 5000 meters?",
+"(5 + 3)/2 * 10 = ?", "Given the array [8, 'Orbit', 'Trajectory', 45, what entry is at index 2?",
+"What is the minimum crew size for the ISS?"];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", 3];
 let candidateAnswers;
 
 
@@ -25,6 +27,7 @@ function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
   candidateAnswer = input.question("Who was the first American woman in space? ");
 } 
+
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
